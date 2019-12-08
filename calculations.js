979588b5eade920439588ebde8e108e7a2a7b857
Joni Arstio <joni.arstio@gmail.com>
2019-12-08 12:32:36 +0200

    added comments and refined functions

--- calculations.js
+++ calculations.js
@@ -1,60 +1,72 @@
+
+// Converter from decimal to binary
 function dec_to_bin() {
     var decConvert = Number(document.getElementById('input1').value);
-    var result = document.getElementById('binPrint1');
-    result.innerHTML += (decConvert.toString(2)) + "<br/>"
+    var result = document.getElementById('result1');
+    result.innerHTML += (decConvert.toString(2)) + "<br/>";
 
 }
 
+// Converter from decimal to octal
 function dec_to_oct() {
     var decConvert = Number(document.getElementById('input2').value);
-    var result = document.getElementById('binPrint2');
+    var result = document.getElementById('result2');
     result.innerHTML += (decConvert.toString(8));
 }
 
+// Converter from decimal to hexadecimal
 function dec_to_hex() {
     var decConvert = Number(document.getElementById('input3').value);
-    var result = document.getElementById('binPrint3');
-    result.innerHTML += (decConvert.toString(16))
+    var result = document.getElementById('result3');
+    result.innerHTML += (decConvert.toString(16));
 }
 
+// Converter from binary to decimal
 function bin_to_dec() {
     var binConvert = Number(document.getElementById('input4').value);
-    var result = document.getElementById('binPrint4');
+    var result = document.getElementById('result4');
     result.innerHTML += (parseInt(binConvert, 2));
 }
 
+// Converter from binary to octal
 function bin_to_oct() {
     var binConvert = Number(document.getElementById('input5').value);
-    var result = document.getElementById('binPrint5');
+    var result = document.getElementById('result5');
     result.innerHTML += (parseInt(binConvert, 2).toString(8));
 }
 
+// Converter from binary to hexadecimal
 function bin_to_hex() {
     var binConvert = Number(document.getElementById('input6').value);
-    var result = document.getElementById('binPrint6');
+    var result = document.getElementById('result6');
     result.innerHTML += (parseInt(binConvert, 2).toString(16));
 }
 
+// Converter from hexadecimal to decimal
 function hex_to_dec() {
     var hexConvert = Number(document.getElementById('input7').value);
-    var result = document.getElementById('binPrint7');
+    var result = document.getElementById('result7');
     result.innerHTML += (parseInt(hexConvert, 16));
 }
 
+// Converter from octal to decimal
 function oct_to_dec() {
     var hexConvert = Number(document.getElementById('input8').value);
-    var result = document.getElementById('binPrint8');
+    var result = document.getElementById('result8');
     result.innerHTML += (parseInt(hexConvert, 8));
 }
 
+// Function for 'hide table' button to toggle hide/show
 function hideTable() {
-    var x = document.getElementById("table");
-        if (x.style.display === "none") {
-            x.style.display = "block";
+    var hide_show = document.getElementById("table");
+        if (hide_show.style.display === "none") {
+            hide_show.style.display = "block";
         } else {
-            x.style.display = "none";
+            hide_show.style.display = "none";
         }
 }
+
+// Function to print decimal table from 0-50
 
 function printDEC(){
           var num = 1;
@@ -64,6 +76,7 @@ function printDEC(){
         }
 }
 
+// Function to print binary table from 0-50
 function printBIN(){
           var num = 1;
               for(var i=0; i<=50; i++){
@@ -71,6 +84,8 @@ function printBIN(){
                     bin.innerHTML += (num*i.toString(2)) + "<br/>" 
         }
 }
+
+// Function to print octal table from 0-50
 function printOCT(){
           var num = 1;
               for(var i=0; i<=50; i++){
@@ -78,7 +93,8 @@ function printOCT(){
                     oct.innerHTML += (num*i.toString(8)) + "<br/>" 
         }
 }
-    
+
+// Function to print hexadecimal table from 0-50
 function printHEX(){
           var num = 1;
               for(var i=0; i<=50; i++){
